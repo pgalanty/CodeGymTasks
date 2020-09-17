aadfe6fa179315c3a6795a0a7fbaf8c54a29d720
pgalanty <pawel.galanty@gmail.com>
2020-09-17 14:13:21 +0200

    two list task added

---
+++ 1.JavaSyntax/src/com/codegym/task/task08/task0807/Solution.java
@@ -0,0 +1,25 @@
+package com.codegym.task.task08.task0807;
+
+import java.util.*;
+
+/* 
+LinkedList and ArrayList
+
+*/
+
+public class Solution {
+    public static Object createArrayList() {
+        //write your code here
+        return new ArrayList<String>();
+    }
+
+    public static Object createLinkedList() {
+        //write your code here
+        return new LinkedList<>();
+    }
+
+    public static void main(String[] args) {
+        Object arrayList = createArrayList();
+        Object linkedList = createLinkedList();
+    }
+}
